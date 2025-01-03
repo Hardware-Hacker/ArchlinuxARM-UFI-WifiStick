f98931429a4a1b3bfa696fd8a62001fd9a99ead0
fashion <fashion@openbfdev.com>
2025-01-03 21:11:23 +0800

    fixup pacman: added system upgrade

--- build.sh
+++ build.sh
@@ -86,7 +86,7 @@ function config_rootfs()
     # Initialize environment
     $chlivedo "pacman-key --init"
     $chlivedo "pacman-key --populate archlinuxarm"
-    $chlivedo "pacman --noconfirm -Syy"
+    $chlivedo "pacman --noconfirm -Syyu"
     $chlivedo "pacman --noconfirm -S arch-install-scripts cloud-guest-utils"
 
     # Install basic rootfs
